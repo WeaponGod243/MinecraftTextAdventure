64b02132429121e27d7a88d3296b86c4845a9c8d
micah <micahaghedo@live.com>
2021-01-13 14:37:31 -0500

    Added new things for executable.

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import os
 from distutils.core import setup
 import py2exe
 
-setup(console=['main.py'])
+
+setup(
+    console=[
+        {
+            "script": "main.py",
+            "icon_resources": [(1, "favicon.ico")]
+        }
+    ],
+    name="MCText Adventure"
+)
