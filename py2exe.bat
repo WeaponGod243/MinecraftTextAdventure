54bc13e89521f96a5afe10ab8ce26fb9897c2a01
micah <micahaghedo@live.com>
2021-01-14 15:13:03 -0500

    Improved distutils functions.

--- py2exe.bat
+++ py2exe.bat
@@ -4,10 +4,14 @@ rem ***** get rid of all the old files in the build folder
 rd /S /Q dist
 
 rem ***** create the exe
-C:\Users\micah\PycharmProjects\MinecraftTextAdventure\venv\Scripts\python setup.py py2exe
+venv\Scripts\python setup.py py2exe
 
 rem **** creates sub in dist
 mkdir dist\sub
 
 rem **** copies sub into dist\sub
 robocopy sub dist\sub /E
+
+cd ..
+
+start zip.bat
